2aaee3ca99250a203d6921c1b7f18c9879511ed0
jimbou <75038393+jimbou@users.noreply.github.com>
2022-04-17 12:18:57 +0300

    Create rapl_pass_final.cpp

---
+++ rapl_energy/rapl_pass_final.cpp
@@ -0,0 +1,79 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+#include <vector>
+
+using namespace llvm;
+
+namespace{
+struct SkeletonPass : public ModulePass{
+    static char ID;
+    Function *monitor;    // this will be the identifier for the print function
+    Function *raplread;   // this will be the identifier for the rapl read function
+
+    SkeletonPass() : ModulePass(ID) {}
+
+    virtual bool runOnModule(Module &M)
+    {
+
+        for(Module::iterator F = M.begin(), E = M.end(); F!= E; ++F)
+        {
+            if(F->getName() == "print"){
+                monitor = cast<Function>(F);  // cast the print function to the monitor pointer
+                continue;
+            }
+            
+            if(F->getName() == "readrapl"){
+                raplread = cast<Function>(F);  // cast the readrapl function to the raplread pointer
+                continue;
+            }
+
+            for(Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB)
+            {
+               
+                    BasicBlock::iterator BI = BB->begin();     // BI will hold the begging point  of the basic block so that we can insert instructions there
+                    BasicBlock::iterator BF = BB->end();
+                    BF--;                                      // BF will hold the ending point  of the basic block so that we can insert instructions there
+                   
+                    errs() << "new basic block\n";
+                    BB->setName(F->getName());                // set the name of the bb to its function name plus a unique number which will be added automatically . for more check read.ME
+                   
+                    errs() << "Basic Block name: " << BB->getName() << "\n";  // print the name of the current BB
+                    errs() << *BB;                                            // print the contents of the bb in the form of llvm IR assembly instructions
+                    
+                    Instruction *inst = &*BI;
+                    IRBuilder<> builder(inst);
+                    Value *v = builder.CreateGlobalStringPtr(BB->getName(), "str");
+                    ArrayRef<Value *> args(v);
+                    Instruction *newInst = CallInst::Create(monitor, args, "");    // create the first insttruction to be added : a call to the print function
+                    BB->getInstList().insert(BI, newInst);                         // we give as argument the name of the bb and we insert it at the beggining f the bb
+              
+                  
+              
+                    Instruction *newInst0 = CallInst::Create(raplread, "");      //create the second instruction to be added : a call to the readrapl function which reads rapl energy
+                    BB->getInstList().insert(BI, newInst0);                       // we insert it at the beggining of the basic block and it will print two energy values
+              
+                    Instruction *newInst1 = CallInst::Create(raplread, "");     //create the third instruction to be added : a call to the readrapl function which reads rapl energy
+                    BB->getInstList().insert(BF, newInst1);                     // we insert it at the end of the basic block and it will print two energy values
+                                       
+
+                
+            }
+        }
+
+        return true;
+    }
+};
+}
+
+char SkeletonPass::ID = 0;
+static RegisterPass<SkeletonPass> X("skeleton", "LLVM IR Instrumentation Pass");
+
+
